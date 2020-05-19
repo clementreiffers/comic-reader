cbde465529a21c11239a60c2bae45d0f2d677636
clementreiffers <reiffersclement@gmail.com>
2020-05-19 13:07:16 +0200

    fonction 'ouverture d'un fichier' ajouté

--- index.py
+++ index.py
@@ -7,35 +7,68 @@ class FenetrePrincipale(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Première Fenêtre')
+        self.filename = ""
 
         self.pageLayout = QVBoxLayout()
         self.buttonLayout = QHBoxLayout()
         self.stackedLayout = QStackedLayout()
 
-        self.livre = c.COMICParser("spidersurf.cbz")
-        self.liste = self.livre.read_book()
+        self.toolbar = QToolBar("Bar d'outils")
+        self.addToolBar(self.toolbar)
 
-        for i in self.liste :
-            self.label = QLabel()
-            self.pixmap= QPixmap("./"+self.livre.name + "/" + i)
-            self.scaledPixmap= self.pixmap.scaledToWidth(self.width() * 0.5)  # Pour prendre 80 % de la largeur
-            self.label.setPixmap(self.scaledPixmap)
-            self.stackedLayout.addWidget(self.label)
+        self.ouvrir = QAction("Ouvrir", self)
+        self.ouvrir.triggered.connect(self.charger)
+        self.ouvrir.setStatusTip("Pour ouvrir un fichier")
+        #self.ouvrir.setIcon(QIcon("editer.png"))
 
-        self.pageLayout.addLayout(self.stackedLayout)
-        self.pageLayout.addLayout(self.buttonLayout) #ajout d'une première sous disposition à pageLayout
+        self.barreDeMenu = self.menuBar()
+        self.menuFichier = self.barreDeMenu.addMenu("&Fichier")
+        self.menuFichier.addAction(self.ouvrir)
+        self.menuFichier.addSeparator()
 
-        self.widgetCentral = QWidget()
-        self.widgetCentral.setLayout(self.pageLayout)
-        self.setCentralWidget(self.widgetCentral)
+        self.menuEdition = self.barreDeMenu.addMenu("&Edition")
+        self.menuEdition.addSeparator()
 
-        #Remplissage des sous-dispositions
-        self.previousnext = ['Previous', 'Next']
-        self.pos = 0
-        for pn in self.previousnext:
-            btn = QPushButton(pn)
-            btn.clicked.connect(self.changerPage)
-            self.buttonLayout.addWidget(btn)
+        self.menuAffichage = self.barreDeMenu.addMenu("&Affichage")
+        self.menuAffichage.addSeparator()
+
+        self.menuLire = self.barreDeMenu.addMenu("&Lire")
+        self.menuLire.addSeparator()
+
+        self.menuAide = self.barreDeMenu.addMenu("&Aide")
+        self.menuAide.addSeparator()
+
+
+        self.ouvrir.setShortcut(QKeySequence("ctrl+o"))
+        #self.ouvrir.setShortcut(QKeySequence("ctrl+o"))
+        self.app()
+    def app(self):
+        if self.filename != "" :
+
+            self.livre = c.COMICParser(self.filename)
+            self.liste = self.livre.read_book()
+
+            for i in self.liste :
+                self.label = QLabel()
+                self.pixmap= QPixmap("./"+self.livre.name + "/" + i)
+                self.scaledPixmap= self.pixmap.scaledToWidth(self.width() * 0.5)  # Pour prendre 80 % de la largeur
+                self.label.setPixmap(self.scaledPixmap)
+                self.stackedLayout.addWidget(self.label)
+
+            self.pageLayout.addLayout(self.stackedLayout)
+            self.pageLayout.addLayout(self.buttonLayout) #ajout d'une première sous disposition à pageLayout
+
+            self.widgetCentral = QWidget()
+            self.widgetCentral.setLayout(self.pageLayout)
+            self.setCentralWidget(self.widgetCentral)
+
+            #Remplissage des sous-dispositions
+            self.previousnext = ['Previous', 'Next']
+            self.pos = 0
+            for pn in self.previousnext:
+                btn = QPushButton(pn)
+                btn.clicked.connect(self.changerPage)
+                self.buttonLayout.addWidget(btn)
 
     @pyqtSlot()
     def changerPage(self):
@@ -47,6 +80,37 @@ class FenetrePrincipale(QMainWindow):
             if self.pos == 0: self.pos = 3
             else :  self.pos-=1
         self.stackedLayout.setCurrentIndex(self.pos)
+
+    def charger(self):
+        dialogue = QFileDialog()
+        self.filename = dialogue.getOpenFileName(self,
+                                                    'Ouvrir fichier',
+
+                                                    filter='Comic Book Zip (*.cbz);;Comic Book Rar (*.cbr)')[0]
+        nom = ""
+        for i in self.filename[::-1]:
+            if i == "/" : break
+            nom += i
+        self.filename = nom[::-1]
+
+        self.app()
+        """
+        if self.filename == ('',''):
+            print('Nom de fichier vide.')
+            return
+
+        fichier = QFile(self.filename)
+        ok = fichier.open(QFile.ReadOnly)
+
+        if ok:
+            print('Le fichier de sauvegarde à été chargé.')
+            flux = QTextStream(fichier)
+            texte = flux.readAll()
+            self.textEdit.setText(texte)
+            fichier.close()
+        else:
+            print('Le fichier de sauvegarde n\'a pas pu être chargé.')
+        """
 
 
 #ces trois lignes c'est pour que ça fonctionne avec spyder
